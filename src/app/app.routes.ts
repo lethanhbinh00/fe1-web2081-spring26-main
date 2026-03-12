b2652837901aafb20b0cf877d53d62bcc144dfe3
lethanhbinh00 <thanhbinh19012006l@example.com>
2026-03-12 13:36:33 +0700

    update lab2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
+import { ProductDetail } from './pages/product.detail/product.detail';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        component: Home,
-    },
-    {
-        path: 'about',
-        component: About,
-    }
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: 'products', component: Products },
+    { path: 'products/:slug', component: ProductDetail },
 ];
---
+++ src/app/pages/products/products.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-products',
+    imports: [],
+    templateUrl: './products.html',
+    styleUrl: './products.css',
+})
+
+export class Products{
+    
+}
