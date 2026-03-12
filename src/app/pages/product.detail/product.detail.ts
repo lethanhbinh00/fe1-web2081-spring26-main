5c36c5b7715fcb6e851ada6fd0d871f44d9f1978
lethanhbinh00 <thanhbinh19012006l@example.com>
2026-03-12 13:44:21 +0700

    update productDetail

--- src/app/pages/product.detail/product.detail.ts
+++ src/app/pages/product.detail/product.detail.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product.detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.detail.html',
   styleUrl: './product.detail.css',
 })
 export class ProductDetail {
-  slug: string | null = null;
+  slug: string = '';
+
+  constructor(private route: ActivatedRoute) {
+    this.slug = this.route.snapshot.params['slug'];
+  }
 }
